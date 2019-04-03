3e8cf8fa9efe0676a8028b2502c6715caa124586
Arseni Bulatski <ancarseni@gmail.com>
2019-04-03 17:05:58 +0300

    CAY-2557 Add java.time.Duration and java.time.Period to supported types

--- cayenne-server/src/main/java/org/apache/cayenne/configuration/server/ServerModule.java
+++ cayenne-server/src/main/java/org/apache/cayenne/configuration/server/ServerModule.java
@@ -54,6 +54,7 @@ import org.apache.cayenne.access.types.CharacterValueType;
 import org.apache.cayenne.access.types.DateType;
 import org.apache.cayenne.access.types.DefaultValueObjectTypeRegistry;
 import org.apache.cayenne.access.types.DoubleType;
+import org.apache.cayenne.access.types.DurationValueType;
 import org.apache.cayenne.access.types.ExtendedType;
 import org.apache.cayenne.access.types.ExtendedTypeFactory;
 import org.apache.cayenne.access.types.FloatType;
@@ -62,6 +63,7 @@ import org.apache.cayenne.access.types.LocalDateTimeValueType;
 import org.apache.cayenne.access.types.LocalDateValueType;
 import org.apache.cayenne.access.types.LocalTimeValueType;
 import org.apache.cayenne.access.types.LongType;
+import org.apache.cayenne.access.types.PeriodValueType;
 import org.apache.cayenne.access.types.ShortType;
 import org.apache.cayenne.access.types.TimeType;
 import org.apache.cayenne.access.types.TimestampType;
@@ -429,6 +431,8 @@ public class ServerModule implements Module {
                 .add(LocalDateValueType.class)
                 .add(LocalTimeValueType.class)
                 .add(LocalDateTimeValueType.class)
+                .add(DurationValueType.class)
+                .add(PeriodValueType.class)
                 .add(CharacterValueType.class);
 
         binder.bind(ValueObjectTypeRegistry.class).to(DefaultValueObjectTypeRegistry.class);
