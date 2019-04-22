8d0fadc895b243fa66108b255d85de9aee1553e8
Nikita Timofeev <stariy95@gmail.com>
2019-04-22 15:45:19 +0300

    Cleanup

--- cayenne-server/src/main/java/org/apache/cayenne/map/DbEntity.java
+++ cayenne-server/src/main/java/org/apache/cayenne/map/DbEntity.java
@@ -25,6 +25,7 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.function.Function;
